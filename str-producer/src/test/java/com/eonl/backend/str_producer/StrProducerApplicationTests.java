341fe56ac1224f99ce70b7e948faab2b0a2abdfc
estevennac <93159877+estevennac@users.noreply.github.com>
2026-02-25 13:42:24 -0500

    Kafka

---
+++ str-producer/src/test/java/com/eonl/backend/str_producer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eonl.backend.str_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
